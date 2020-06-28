8d1220e8265dfbe9ccef40ee47847dbeb1d2ec39
Qin Liu <qin.liu16@imperial.ac.uk>
2020-06-28 18:05:41 +0800

    LG analytic solution phase, interferogram, intensity plot

--- LG mode plot.py
+++ LG mode plot.py
@@ -26,8 +26,91 @@ w = w_0 * np.sqrt(1 + (z/z_R)**2)
 
 R = z + z_R**2/z
 
-Guoy_phase = np.arctan(z/z_R)
 
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def Lagpolynomial(p,l,x):
+    if p == 0:
+        return 1
+    if p == 1:
+        return (l+1)-x
+    if p == 2:
+        return (l+1)*(l+2)/factorial(2) - (l+2)*x + x**2/factorial(2)
+    else:
+        return 0
+
+def LG_pl(p,l,x,y):
+    r = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    Guoy_phase = (2*p + l + 1)*np.arctan2(z,z_R)
+    Kpl = (-1)**p*np.sqrt(2/np.pi)*np.sqrt(factorial(p)/factorial(p+l))
+    
+    return Kpl/w * Lagpolynomial(p,l,2*r**2/w**2)*(np.sqrt(2)*r/w)**l*np.exp(-l*phi*1j)*np.exp(-r**2/w**2)*np.exp(k*r**2/(2*R)*1j)*np.exp(-Guoy_phase*1j)
+
+def LG_phase(p,l,x,y):
+    r = np.sqrt(x**2 + y**2)
+    phi_m = np.arctan2(y, x)
+    Guoy_phase = (2*p + l + 1)*np.arctan2(z,z_R)
+    phase = l*phi_m + k*r**2/(2*R) - Guoy_phase
+    #phase = k*r**2/(2*R)
+    
+    while (np.ptp(phase) > 2*np.pi):
+    
+        phase = np.where(phase <= np.pi, phase, phase - 2*np.pi)
+        phase = np.where(phase >= -np.pi, phase, phase + 2*np.pi)
+        
+    
+    else:
+ 
+        return phase
+def E_linear(x,theta_inc):
+    return 20*np.exp((k*np.cos(theta_inc)*z + k*np.sin(theta_inc)*x)*1j)
+
+def inteferogram(x,y,p,l,theta_inc):
+    E_int = E_linear(x,theta_inc)+ LG_pl(p,l,x,y)
+    Er = E_int.real
+    Eim = E_int.imag
+    
+    return np.sqrt(Er**2+Eim**2)
+           
+    
+p = 0
+l = 1
+theta_inc = 0
+
+#inteferogram plot to see fork pattern
+x = np.arange(-50*10**(-5), 50*10**(-5), 50*10**(-8))
+y = np.arange(-50*10**(-5), 50*10**(-5), 50*10**(-8))
+X, Y = np.meshgrid(x, y)
+plt.imshow(inteferogram(X,Y,p,l,theta_inc))
+plt.colorbar()
+
+
+'''
+#close look
+x = np.arange(-50*10**(-6), 50*10**(-6), 50*10**(-8))
+y = np.arange(-50*10**(-6), 50*10**(-6), 50*10**(-8))
+X, Y = np.meshgrid(x, y)
+plt.imshow(LG_phase(p, l, X, Y))
+plt.colorbar()
+'''
+
+'''
+#further away from centre
+
+x = np.arange(-20*10**(-5), 20*10**(-5), 50*10**(-8))
+y = np.arange(-20*10**(-5), 20*10**(-5), 50*10**(-8))
+X, Y = np.meshgrid(x, y)
+plt.imshow(LG_phase(p,l,X,Y))
+plt.colorbar()
+'''
+'''
 fig = plt.figure()
 ax = Axes3D(fig)
 x = np.arange(-50*10**(-4), 50*10**(-4), 50*10**(-7))
@@ -35,14 +118,22 @@ y = np.arange(-50*10**(-4), 50*10**(-4), 50*10**(-7))
 X, Y = np.meshgrid(x, y)
 
 
-r = np.sqrt(X**2 + Y**2)
 
-E = (w_0 * np.sqrt(2)/w**2)*(X + Y*1j)*np.exp(-r**2/w**2)*np.exp(k*r**2/(2*R)*1j)*np.exp(-2*np.arctan(z/z_R)*1j)
 
-Er = E.real
-Eim = E.imag
+
+#Kpl = (-1)**p*np.sqrt(2/np.pi)*np.sqrt(factorial(p)/factorial(p+l))
+
+#LG_pl = Kpl/w * Lagpolynomial(p,l,2*r**2/w**2)*(np.sqrt(2)*r/w)**l*np.exp(-l*phi*1j)*np.exp(-r**2/w**2)*np.exp(k*r**2/(2*R)*1j)*np.exp(-Guoy_phase*1j)
+
+#E = (w_0 * np.sqrt(2)/w**2)*(X + Y*1j)*np.exp(-r**2/w**2)*np.exp(k*r**2/(2*R)*1j)*np.exp(-2*np.arctan(z/z_R)*1j)
+
+#phase = l*phi 
+#+ k*r**2/(2*R) - Guoy_phase
+Er = LG_pl(p,l,X,Y).real
+Eim = LG_pl(p,l,X,Y).imag
 I = np.sqrt(Er**2+Eim**2)
 
 
-ax.plot_surface(X, Y, I, cmap=cm.coolwarm,
+ax.plot_surface(X, Y, LG_phase(p,l,X,Y), cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
+'''
--- Vortex beam generation.py
+++ Vortex beam generation.py
@@ -15,9 +15,9 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 #use the data from Vaity 2013
 Lambda = 6.32*10**(-7)
 k = 2*np.pi/Lambda
-d = 0.05*10**(-3)
+d = 0.001*10**(-3)
 theta = 0.04
-z = 0.2
+z = 0.8
 w_0 = np.sqrt(Lambda*d/(np.pi*theta))
 z_R = np.pi*w_0**2/Lambda
 
@@ -26,35 +26,68 @@ w = w_0 * np.sqrt(1 + (z/z_R)**2)
 
 R = z + z_R**2/z
 
-Guoy_phase = np.arctan(z/z_R)
+Guoy_phase = np.arctan2(z,z_R)
 
+def E_tilt(x,y,d,theta):
+    return (w/w_0)*np.exp(-(1/w**2)*((x + z*theta)**2 + (y + d)**2)) * np.exp(-Guoy_phase*1j)\
+        *np.exp(k*z*1j + k/(2*R)*((x + z*theta)**2 + (y + d)**2)*1j)*np.exp(-k*(x*theta + z*theta**2/2)*1j)
+
+def I(x,y):
+    E_vortex = E_tilt(X,Y,d,-theta) + E_tilt(X,Y,-d,theta)*np.exp(np.pi*1j)
+    Er = E_vortex.real
+    Eim = E_vortex.imag
+    
+    return np.sqrt(Er**2+Eim**2)
+
+def I_analytic(x,y):
+
+    h = k * z
+    m = k * (x**2 + y**2) / (2 * R)
+    E_g = (w/w_0) * np.exp(-(x**2 + y**2) / w**2) * np.exp(-(Guoy_phase)*1j) * np.exp(h*1j + m*1j)
+
+    a = k * (x * theta * (1 - (z / R)) + d * y / R)
+    b = (2 / w**2) * (X * z * theta - d * Y)
+    c = k / (2*R)
+    n = 1/ w**2 
+    f = z**2 * theta**2 + d**2
+    g = k * z * theta**2 / 2
+    E = 2*1j * np.sin(a - b*1j) * E_g * np.exp((c*1j - n ) * f) * np.exp(-g*1j)
+    Er = E.real
+    Eim = E.imag
+    
+    return np.sqrt(Er**2+Eim**2)
+
+def E_linear(x,theta_inc):
+    return 20*np.exp((k*np.cos(theta_inc)*z + k*np.sin(theta_inc)*x)*1j)
+
+def inteferogram(x,y,theta_inc):
+    E_int = E_linear(x,theta_inc)+ E_tilt(x,y)
+    Er = E_int.real
+    Eim = E_int.imag
+    
+    return np.sqrt(Er**2+Eim**2)
+
+
+theta_inc = 0.01
 
 #Generation of Vortex beam
-
-fig = plt.figure()
-ax = Axes3D(fig)
 x = np.arange(-50*10**(-3), 50*10**(-3), 50*10**(-6))
 y = np.arange(-50*10**(-3), 50*10**(-3), 50*10**(-6))
 X, Y = np.meshgrid(x, y)
-
-h = k * z
-m = k * (X**2 + Y**2) / (2 * R)
-E_g = (w/w_0) * np.exp(-(X**2 + Y**2) / w**2) * np.exp(-(Guoy_phase)*1j) * np.exp(h*1j + m*1j)
-
-a = k * (X * theta * (1 - (z / R)) + d * Y / R)
-b = (2 / w**2) * (X * z * theta - d * Y)
-c = k / (2*R)
-n = 1/ w**2 
-f = z**2 * theta**2 + d**2
-g = k * z * theta**2 / 2
-E = 2*1j * np.sin(a - b*1j) * E_g * np.exp((c*1j - n ) * f) * np.exp(-g*1j)
+plt.imshow(I_analytic(X,Y))
+plt.colorbar()
 
 
-#E_g1 = np.exp(-(X**2 + Y**2) / w**2)
-Er = E.real
-Eim = E.imag
-I = np.sqrt(Er**2+Eim**2)
+'''
+
+fig = plt.figure()
+ax = Axes3D(fig)
+x = np.arange(-80*10**(-4), 80*10**(-4), 50*10**(-7))
+y = np.arange(-80*10**(-4), 80*10**(-4), 50*10**(-7))
+X, Y = np.meshgrid(x, y)
+'''
 
 
-ax.plot_surface(X, Y, I, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+
+
+#ax.plot_surface(X, Y, I(X,Y,z,d,theta), cmap=cm.coolwarm, linewidth=0, antialiased=False)
